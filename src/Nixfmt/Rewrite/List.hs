78e9599a61301290f96a036155de213c5af46d0b
Connor Baker <connorbaker01@gmail.com>
2025-06-06 21:27:12 -0700

    wip
    Signed-off-by: Connor Baker <connorbaker01@gmail.com>

--- src/Nixfmt/Rewrite/List.hs
+++ src/Nixfmt/Rewrite/List.hs
@@ -12,71 +12,97 @@ module Nixfmt.Rewrite.List (
 )
 where
 
-import Control.Monad.Fix (fix)
+import Data.Function (on)
 import Nixfmt.Pretty ()
 import Nixfmt.Types (
   Ann (..),
   Expression (..),
   Item (..),
   Items (..),
+  Selector (..),
+  SimpleSelector (..),
   Term (..),
   Token (..),
+  ann,
  )
+import Text.Megaparsec (pos1)
+
+builtinsConcatLists :: Expression
+builtinsConcatLists =
+  Term
+    ( Selection
+        (Token $ ann pos1 (Identifier "builtins"))
+        [Selector (Just $ ann pos1 TDot) (IDSelector $ ann pos1 (Identifier "concatLists"))]
+        Nothing
+    )
+
+mkParenthesized :: Expression -> Term
+mkParenthesized expr =
+  Parenthesized
+    (ann pos1 TParenOpen)
+    expr
+    (ann pos1 TParenClose)
+
+mkList :: [Expression] -> Term
+mkList exprs =
+  List
+    (ann pos1 TBrackOpen)
+    (Items $ (Item . mkParenthesized) <$> exprs)
+    (ann pos1 TBrackClose)
 
 rewriteListConcatenation :: Expression -> Expression
 rewriteListConcatenation = rewriteExpression
 
 rewriteTerm :: Term -> Term
 rewriteTerm = \case
-  (List open items close) ->
-    List
-      open
-      -- TODO: Why was I unable to write this using just fmap?
-      ( Items
-          ( map
-              ( \case
-                  (Item term) -> Item (rewriteTerm term)
-                  other -> other
-              )
-              (unItems items)
-          )
-      )
-      close
+  List open items close -> List open (rewriteTerm <$> items) close
   -- TODO: Set
   -- TODO: Selection
-  (Parenthesized open expr close) -> Parenthesized open (rewriteExpression expr) close
+  Parenthesized open expr close -> Parenthesized open (rewriteExpression expr) close
   other -> other
+
+mkConcatListsApplication :: [Expression] -> Expression
+mkConcatListsApplication exprs =
+  Application
+    builtinsConcatLists
+    (Term $ mkList exprs)
+
+gatherConcatenationExpressions :: Expression -> [Expression]
+gatherConcatenationExpressions = \case
+  Operation left Ann{value = TConcat} right ->
+    ((<>) `on` gatherConcatenationExpressions) left right
+  expr -> [expr]
 
 rewriteExpression :: Expression -> Expression
 rewriteExpression = \case
-  (Term term) -> Term (rewriteTerm term)
-  (With with expr0 semicolon expr1) ->
+  Term term -> Term (rewriteTerm term)
+  With with expr0 semicolon expr1 ->
     With with (rewriteExpression expr0) semicolon (rewriteExpression expr1)
-  (Let let_ items in_ body) ->
+  Let let_ items in_ body ->
     -- TODO: Handle items
     Let let_ items in_ (rewriteExpression body)
   assertion@(Assert{}) -> assertion -- Don't rewrite asserts.
-  (If if_ expr0 then_ expr1 else_ expr2) ->
+  If if_ expr0 then_ expr1 else_ expr2 ->
     If if_ (rewriteExpression expr0) then_ (rewriteExpression expr1) else_ (rewriteExpression expr2)
-  (Abstraction param colon body) ->
+  Abstraction param colon body ->
     -- TODO: Ignoring param, though there could be rewrites applied to defaults
     Abstraction param colon (rewriteExpression body)
-  (Application g a) -> Application (rewriteExpression g) (rewriteExpression a)
-  (Operation left op right) -> case op of
+  Application g a -> Application (rewriteExpression g) (rewriteExpression a)
+  Operation left op right -> case op of
     Ann{value = TConcat} ->
+      -- Concatenation is right-associative.
+      -- Need to fold the left and right sides of the concatenation into a single expression using concatLists.
       case (left, right) of
-        -- Rewrite concatenation of lists with the empty list
-        (Term (List _ (Items []) _), _) -> right
-        (_, Term (List _ (Items []) _)) -> left
-        -- Rewrite concatenation of lists of values as a single list
+        -- Identity
+        (Term (List _ (Items []) _), _) -> rewriteExpression right
+        (_, Term (List _ (Items []) _)) -> rewriteExpression left
+        -- Simplification of known lists
         (Term (List openLeft (Items itemsLeft) closeLeft), Term (List _openRight (Items itemsRight) _closeRight)) ->
-          Term (List openLeft (Items (itemsLeft <> itemsRight)) closeLeft)
-        -- TODO: Rewrite concatenation of lists of values and values with builtins.concatLists
-        -- Otherwise, rewrite both sides
-        _ -> Operation (rewriteExpression left) op (rewriteExpression right)
+          Term (List openLeft (rewriteTerm <$> Items (itemsLeft <> itemsRight)) closeLeft)
+        _ -> mkConcatListsApplication $ ((<>) `on` gatherConcatenationExpressions) left right
     _ -> Operation (rewriteExpression left) op (rewriteExpression right)
-  (MemberCheck name dot sels) ->
+  MemberCheck name dot sels ->
     -- TODO: Does not handle potential rewrites in selectors
     MemberCheck (rewriteExpression name) dot sels
-  (Negation not_ expr) -> Negation not_ (rewriteExpression expr)
-  (Inversion tilde expr) -> Inversion tilde (rewriteExpression expr)
+  Negation not_ expr -> Negation not_ (rewriteExpression expr)
+  Inversion tilde expr -> Inversion tilde (rewriteExpression expr)
